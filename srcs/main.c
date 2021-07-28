519b0d14c8ae3fe5efe266fe3dc9388255090306
Parallels <you@example.com>
2021-07-29 00:33:50 +0900

    done map

--- srcs/main.c
+++ srcs/main.c
@@ -7,28 +7,90 @@
 #define UP 65362
 #define RIGHT 65363
 #define DOWN 65364
+#define ESC 65307
+
+enum	e_piece_of_map
+{
+	EMPTY,
+	WALL,
+	COLL,
+	EXIT,
+	PLAYER,
+	PIECE_NUM,
+};
+
+enum	e_coordinates
+{
+	X,
+	Y,
+	COOR_NUM,
+};
 
 typedef struct	s_data {
 	void	*mlx;
 	void	*mlx_win;
-	void	*base_img;
-	int		base_width;
-	int		base_height;
-	void	*move_img;
-	int		move_width;
-	int		move_height;
-	int		wid_cnt;
-	int		hei_cnt;
-	int		move_x;
-	int		move_y;
+	void	*img[PIECE_NUM];
+	int		img_width;
+	int		img_height;
+	int		w_cnt;
+	int		h_cnt;
+	int		player[COOR_NUM];
+	int		**map;
 }				t_data;
+
+void	initialize_data(t_data *data)
+{
+	data->mlx = mlx_init();
+	data->img[EMPTY] = mlx_xpm_file_to_image\
+		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+	data->img[WALL] = mlx_xpm_file_to_image\
+		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+	data->img[COLL] = mlx_xpm_file_to_image\
+		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+	data->img[EXIT] = mlx_xpm_file_to_image\
+		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+	data->img[PLAYER] = mlx_xpm_file_to_image\
+		(data->mlx, "./xpm/ship02.xpm", &(data->img_width), &(data->img_height));
+	data->mlx_win = mlx_new_window(data->mlx, \
+		data->w_cnt * data->img_width, data->h_cnt * data->img_height, "Hello world!");
+}
+
+void	free_2d_array(int **array, int until)
+{
+	while (--until >= 0)
+		free(array[until]);
+	free(array);
+}
+
+void	destroy_img(t_data *data)
+{
+	int i;
+
+	i = -1;
+	while (++i < PIECE_NUM)
+		mlx_destroy_image(data->mlx, data->img[i]);
+}
+
+void	put_2d_array(t_data *data)
+{
+	int	x;
+	int y;
+
+	x = -1;
+	while (++x < data->w_cnt)
+	{
+		y = -1;
+		while (++y < data->h_cnt)
+			printf("(%d, %d)->%d\n", x, y, data->map[x][y]);
+	}
+}
 
 int	close_win(t_data *data)
 {
-	mlx_destroy_image(data->mlx, data->base_img);
-	mlx_destroy_image(data->mlx, data->move_img);
 	mlx_destroy_window(data->mlx, data->mlx_win);
+	destroy_img(data);
 	mlx_destroy_display(data->mlx);
+	free_2d_array(data->map, data->h_cnt);
 	free(data->mlx);
 	exit(0);
 	return (0);
@@ -36,54 +98,75 @@ int	close_win(t_data *data)
 
 void	create_map(t_data *data)
 {
-	int		x_cnt = 0;
-	int		y_cnt = 0;
+	int	x;
+	int	y;
 
 //	mlx_clear_window(data->mlx, data->mlx_win);
-	while (y_cnt < data->hei_cnt)
+	x = -1;
+	while (++x < data->w_cnt)
 	{
-		x_cnt = 0;
-		while (x_cnt < data->wid_cnt)
+		y = -1;
+		while (++y < data->h_cnt)
 		{
-			mlx_put_image_to_window(data->mlx, data->mlx_win, data->base_img, x_cnt * data->base_width, y_cnt * data->base_height);
-			x_cnt++;
+			mlx_put_image_to_window\
+			(data->mlx, data->mlx_win, data->img[data->map[x][y]], \
+			x * data->img_width, y * data->img_height);
 		}
-		y_cnt++;
 	}
-	mlx_put_image_to_window(data->mlx, data->mlx_win, data->move_img, \
-					data->move_x * data->base_width, data->move_y * data->base_height);
+	mlx_put_image_to_window\
+	(data->mlx, data->mlx_win, data->img[PLAYER], \
+	data->player[X] * data->img_width, data->player[Y] * data->img_height);
 }
 
 int	key_hook(int keycode, t_data *data)
 {
-	if (keycode == LEFT && 0 < data->move_x)
-		data->move_x--;
-	if (keycode == RIGHT && data->move_x < data->wid_cnt - 1)
-		data->move_x++;
-	if (keycode == UP && 0 < data->move_y)
-		data->move_y--;
-	if (keycode == DOWN && data->move_y < data->hei_cnt - 1)
-		data->move_y++;
-	if (keycode == 65307)
+	if (keycode == LEFT && 0 < data->player[X])
+		data->player[X]--;
+	if (keycode == RIGHT && data->player[X] < data->w_cnt - 1)
+		data->player[X]++;
+	if (keycode == UP && 0 < data->player[Y])
+		data->player[Y]--;
+	if (keycode == DOWN && data->player[Y] < data->h_cnt - 1)
+		data->player[Y]++;
+	if (keycode == ESC)
 		close_win(data);
 	create_map(data);
 	return (keycode);
 }
 
+void	make_2d_array(t_data *data)
+{
+	int	x;
+	int	y;
+
+	data->map = (int **)malloc(sizeof(int *) * data->w_cnt);
+	if (!(data->map))
+		exit(1);
+	x = -1;
+	while (++x < data->w_cnt)
+	{
+		(data->map)[x] = (int *)malloc(sizeof(int) * data->h_cnt);
+		if (!(data->map)[x])
+		{
+			free_2d_array(data->map, x);
+			exit (1);
+		}
+		y = -1;
+		while (++y < data->h_cnt)
+			(data->map)[x][y] = EMPTY;
+	}
+}
+
 int	main(void)
 {
 	t_data	data;
-	char	*base_path = "./xpm/ship.xpm";
-	char	*move_path = "./xpm/ship02.xpm";
 
-	data.wid_cnt = 5;
-	data.hei_cnt = 5;
-	data.move_x = 0;
-	data.move_y = 0;
-	data.mlx = mlx_init();
-	data.base_img = mlx_xpm_file_to_image(data.mlx, base_path, &data.base_width, &data.base_height);
-	data.move_img = mlx_xpm_file_to_image(data.mlx, move_path, &data.move_width, &data.move_height);
-	data.mlx_win = mlx_new_window(data.mlx, data.base_width * data.wid_cnt, data.base_height * data.hei_cnt, "Hello world!");
+	data.w_cnt = 5;
+	data.h_cnt = 5;
+	data.player[X] = 0;
+	data.player[Y] = 0;
+	make_2d_array(&data);
+	initialize_data(&data);
 	create_map(&data);
 	mlx_key_hook(data.mlx_win, key_hook, &data);
 	mlx_hook(data.mlx_win, 33, 1L << 17, close_win, &data);
