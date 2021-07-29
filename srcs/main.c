7a257bf4090c344862fa70cb702b004ca50b84f9
Parallels <you@example.com>
2021-07-29 19:42:45 +0900

    done move_next_or_not

--- srcs/main.c
+++ srcs/main.c
@@ -35,6 +35,8 @@ typedef struct	s_data {
 	int		w_cnt;
 	int		h_cnt;
 	int		player[COOR_NUM];
+	int		coll[COOR_NUM];
+	int		coll_num;
 	int		**map;
 }				t_data;
 
@@ -42,17 +44,18 @@ void	initialize_data(t_data *data)
 {
 	data->mlx = mlx_init();
 	data->img[EMPTY] = mlx_xpm_file_to_image\
-		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+		(data->mlx, "./xpm/forest.xpm", &(data->img_width), &(data->img_height));
 	data->img[WALL] = mlx_xpm_file_to_image\
-		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+		(data->mlx, "./xpm/tree.xpm", &(data->img_width), &(data->img_height));
 	data->img[COLL] = mlx_xpm_file_to_image\
-		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+		(data->mlx, "./xpm/old-man.xpm", &(data->img_width), &(data->img_height));
 	data->img[EXIT] = mlx_xpm_file_to_image\
-		(data->mlx, "./xpm/ship.xpm", &(data->img_width), &(data->img_height));
+		(data->mlx, "./xpm/escape.xpm", &(data->img_width), &(data->img_height));
 	data->img[PLAYER] = mlx_xpm_file_to_image\
-		(data->mlx, "./xpm/ship02.xpm", &(data->img_width), &(data->img_height));
+		(data->mlx, "./xpm/monster.xpm", &(data->img_width), &(data->img_height));
 	data->mlx_win = mlx_new_window(data->mlx, \
 		data->w_cnt * data->img_width, data->h_cnt * data->img_height, "Hello world!");
+	data->coll_num = 0;
 }
 
 void	free_2d_array(int **array, int until)
@@ -115,32 +118,37 @@ void	create_map(t_data *data)
 	}
 }
 
+void	move_next_or_not(int next_x, int next_y, t_data *data)
+{
+	if (next_x < 0 || data->w_cnt - 1 < next_x || next_y < 0 || data->h_cnt - 1 < next_y)
+		return ;
+	if (data->map[next_x][next_y] == WALL)
+		return ;
+	if (data->map[next_x][next_y] == EXIT)
+	{
+		if (2 < data->coll_num)
+			close_win(data);
+		else
+			return ;
+	}
+	if (data->map[next_x][next_y] == COLL)
+		data->coll_num++;
+	data->map[data->player[X]][data->player[Y]] = EMPTY;
+	data->map[next_x][next_y] = PLAYER;
+	data->player[X] = next_x;
+	data->player[Y] = next_y;
+}
+
 int	key_hook(int keycode, t_data *data)
 {
-	if (keycode == LEFT && 0 < data->player[X])
-	{
-		(data->map)[data->player[X]][data->player[Y]] = EMPTY;
-		data->player[X]--;
-		(data->map)[data->player[X]][data->player[Y]] = PLAYER;
-	}
-	if (keycode == RIGHT && data->player[X] < data->w_cnt - 1)
-	{
-		(data->map)[data->player[X]][data->player[Y]] = EMPTY;
-		data->player[X]++;
-		(data->map)[data->player[X]][data->player[Y]] = PLAYER;
-	}
-	if (keycode == UP && 0 < data->player[Y])
-	{
-		(data->map)[data->player[X]][data->player[Y]] = EMPTY;
-		data->player[Y]--;
-		(data->map)[data->player[X]][data->player[Y]] = PLAYER;
-	}
-	if (keycode == DOWN && data->player[Y] < data->h_cnt - 1)
-	{
-		(data->map)[data->player[X]][data->player[Y]] = EMPTY;
-		data->player[Y]++;
-		(data->map)[data->player[X]][data->player[Y]] = PLAYER;
-	}
+	if (keycode == LEFT)
+		move_next_or_not(data->player[X] - 1, data->player[Y], data);
+	if (keycode == RIGHT)
+		move_next_or_not(data->player[X] + 1, data->player[Y], data);
+	if (keycode == UP)
+		move_next_or_not(data->player[X], data->player[Y] - 1, data);
+	if (keycode == DOWN)
+		move_next_or_not(data->player[X], data->player[Y] + 1, data);
 	if (keycode == ESC)
 		close_win(data);
 	create_map(data);
@@ -153,32 +161,36 @@ void	make_2d_array(t_data *data)
 	int	y;
 
 	data->map = (int **)malloc(sizeof(int *) * data->w_cnt);
-	if (!(data->map))
+	if (!data->map)
 		exit(1);
 	x = -1;
 	while (++x < data->w_cnt)
 	{
-		(data->map)[x] = (int *)malloc(sizeof(int) * data->h_cnt);
-		if (!(data->map)[x])
+		data->map[x] = (int *)malloc(sizeof(int) * data->h_cnt);
+		if (!data->map[x])
 		{
 			free_2d_array(data->map, x);
 			exit (1);
 		}
 		y = -1;
 		while (++y < data->h_cnt)
-			(data->map)[x][y] = EMPTY;
-		data->player[X] = 0;
-		data->player[Y] = 0;
-		(data->map)[data->player[X]][data->player[Y]] = PLAYER;
+			data->map[x][y] = EMPTY;
 	}
+	data->player[X] = 0;
+	data->player[Y] = 0;
+	data->coll[X] = 4;
+	data->coll[Y] = 4;
+	data->map[data->player[X]][data->player[Y]] = PLAYER;
+	data->map[data->coll[X]][data->coll[Y]] = COLL;
+	data->map[7][7] = WALL;
 }
 
 int	main(void)
 {
 	t_data	data;
 
-	data.w_cnt = 5;
-	data.h_cnt = 5;
+	data.w_cnt = 10;
+	data.h_cnt = 10;
 	make_2d_array(&data);
 	initialize_data(&data);
 	create_map(&data);
